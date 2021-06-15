d6ef29d04d09006d92400a151702d81b0d87e1cf
hanbekova-j <70514331+juliakalina@users.noreply.github.com>
2021-06-15 09:18:48 +0300

    Update dialog.h

--- dialog.h
+++ dialog.h
@@ -4,7 +4,6 @@ char* get_str(void);
 int _feof(void);
 char* fget_str(FILE* fd);
 void get_int(unsigned int* key1);
-//int dialog(void);
 int dialog(const char* msgs[], int n);
 Vertex* create_vertex(void);
 void DFS_SHELL(Vertex** list);
