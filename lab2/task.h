618edb516ab5bb2b20c523e76708ded3afd8d8bf
hanbekova-j <70514331+juliakalina@users.noreply.github.com>
2021-10-11 00:02:06 +0300

    Create task.h

---
+++ lab2/task.h
@@ -0,0 +1,11 @@
+#ifndef __TASK_H_INCLUDED__
+#define __TASK_H_INCLUDED__
+
+typedef struct _tag_task
+{
+	int angle;
+	int members_num;
+	double res;
+} task_t;
+
+#endif /* !__TASK_H_INCLUDED__ */
