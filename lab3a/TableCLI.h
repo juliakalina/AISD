56a06566e880b1140a1b38c6cfe38df4113cdea2
hanbekova-j <70514331+juliakalina@users.noreply.github.com>
2021-10-16 23:00:54 +0300

    Add files via upload

---
+++ lab3a/TableCLI.h
@@ -0,0 +1,13 @@
+#ifndef __TABLE_CLI_H_INCLUDED__
+#define __TABLE_CLI_H_INCLUDED__
+
+#include "Table.h"
+
+int dialog(char* msgs[], int n);
+
+void CLIAdd(Table* table); 
+void CLIFind(Table* table);
+void CLIRemove(Table* table);
+void CLIPrint(Table* table);
+
+#endif /* !__TABLE_CLI_H_INCLUDED__ */
